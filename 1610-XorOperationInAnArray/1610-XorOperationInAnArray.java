5d5da2e60f62bbc7c0253558b6b81daf1cbf2192
Raaghavendra A M <raaghavendra71@gmail.com>
2026-04-04 15:44:46 +0530

    Create 1610-XorOperationInAnArray

---
+++ 1610-XorOperationInAnArray/1610-XorOperationInAnArray.java
@@ -0,0 +1,12 @@
+// Last updated: 04/04/2026, 15:44:46
+class Solution {
+    public int xorOperation(int n, int start) {
+        int[] nums = new int[n];
+        nums[0] = start;
+        for (int i = 1; i < n; i++){
+            nums[i] = start+2*i;
+            nums[i] ^= nums[i-1];
+        }
+    return nums[n-1];
+    }
+}
