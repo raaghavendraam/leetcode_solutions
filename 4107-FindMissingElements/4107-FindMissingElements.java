85cf17f44f9fd0d3cc065348a832c8cd146661d9
Raaghavendra A M <raaghavendra71@gmail.com>
2026-08-05 15:14:06 +0530

    Create 4107-FindMissingElements

---
+++ 4107-FindMissingElements/4107-FindMissingElements.java
@@ -0,0 +1,19 @@
+// Last updated: 05/08/2026, 15:14:06
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = 101;
+        int max = -1;
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i <n; i++) {
+            max = Math.max(max,nums[i]);
+            min = Math.min(min,nums[i]);
+            set.add(nums[i]);
+        }
+        for (int i = min; i <= max; i++) {
+            if(!set.contains(i)) ans.add(i);
+        }
+        return ans;
+    }
+}
