510f1e87d8ed05f35fece91b16d1f7b6cfe78826
Raaghavendra A M <raaghavendra71@gmail.com>
2026-09-07 08:45:24 +0530

    Create 1636-SortArrayByIncreasingFrequency

---
+++ 1636-SortArrayByIncreasingFrequency/1636-SortArrayByIncreasingFrequency.java
@@ -0,0 +1,34 @@
+// Last updated: 07/09/2026, 08:45:23
+class Solution {
+     public int[] frequencySort(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        PriorityQueue<pair> pq = new PriorityQueue<>();
+        for (int ele : nums) {
+            if(map.containsKey(ele)) map.put(ele,(map.get(ele))+1);
+            else map.put(ele,1);
+        }
+        for (int ele : map.keySet()) {
+            pq.add(new pair(ele, map.get(ele)));
+        }
+        int idx = 0;
+        int[] ans = new int[nums.length];
+        while(pq.size()>0){
+            pair temp = pq.remove();
+            for(int i = 0;i<temp.freq;i++){
+                ans[idx++]=temp.ele;
+            }
+        }
+        return ans;
+    }class pair implements Comparable<pair>{
+    int ele;
+    int freq;
+    pair(int ele,int freq){
+        this.ele = ele;
+        this.freq = freq;
+    }
+    public int compareTo(pair s){
+        if(this.freq==s.freq) return s.ele-this.ele;
+        return this.freq-s.freq;
+    }
+}
+}
