5e104edf339f433c8c0ca28a1f4cd2529fe6a79d
Raaghavendra A M <raaghavendra71@gmail.com>
2026-09-07 08:44:14 +0530

    Create 2958-LengthOfLongestSubarrayWithAtMostKFrequency

---
+++ 2958-LengthOfLongestSubarrayWithAtMostKFrequency/2958-LengthOfLongestSubarrayWithAtMostKFrequency.java
@@ -0,0 +1,28 @@
+// Last updated: 07/09/2026, 08:44:13
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int i =0,j =0,n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max = 0;
+        while (i<n&&j<n&&i<=j) {
+            int ele = nums[j];
+            if(map.containsKey(ele)){
+                if(map.get(ele)<k){
+                    map.put(ele,map.get(ele)+1);
+                    j++;
+                }
+                else{
+                    int len = j-i;
+                    max = Math.max(max, len);
+                    map.put(nums[i], map.get(nums[i]) - 1);
+                    i++;
+                }
+            }
+            else{
+                map.put(ele, 1);
+                j++;
+            }
+        }
+        return Math.max(max,j-i);
+    }
+}
