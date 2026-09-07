95425802ee0636922b75337632878f698a19e2bd
Raaghavendra A M <raaghavendra71@gmail.com>
2026-09-07 08:44:03 +0530

    Create 3020-FindTheMaximumNumberOfElementsInSubset

---
+++ 3020-FindTheMaximumNumberOfElementsInSubset/3020-FindTheMaximumNumberOfElementsInSubset.java
@@ -0,0 +1,49 @@
+// Last updated: 07/09/2026, 08:44:02
+class Solution {
+    public int maximumLength(int[] nums) {
+        HashMap<Long, Integer> freq = new HashMap<>();
+
+        for (int x : nums) {
+            freq.put((long) x, freq.getOrDefault((long) x, 0) + 1);
+        }
+
+        int ans = 1;
+
+        // Handle 1 separately
+        if (freq.containsKey(1L)) {
+            int cnt = freq.get(1L);
+            ans = Math.max(ans, (cnt % 2 == 0) ? cnt - 1 : cnt);
+        }
+
+        for (long x : freq.keySet()) {
+            if (x == 1) continue;
+
+            long cur = x;
+            int len = 0;
+
+            while (true) {
+                int cnt = freq.getOrDefault(cur, 0);
+
+                if (cnt >= 2) {
+                    len += 2;
+                } else if (cnt == 1) {
+                    len += 1;
+                    break;
+                } else {
+                    len -= 1;
+                    break;
+                }
+
+                // Prevent overflow
+                if (cur > (long) Math.sqrt(Long.MAX_VALUE))
+                    break;
+
+                cur *= cur;
+            }
+
+            ans = Math.max(ans, len);
+        }
+
+        return ans;
+    }
+}
