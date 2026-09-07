68b62cfa4aa1ef96707375a86014718dc6be4752
Raaghavendra A M <raaghavendra71@gmail.com>
2026-09-07 08:43:53 +0530

    Create 3310-RemoveMethodsFromProject

---
+++ 3310-RemoveMethodsFromProject/3310-RemoveMethodsFromProject.java
@@ -0,0 +1,51 @@
+// Last updated: 07/09/2026, 08:43:52
+
+
+class Solution {
+
+    public void dfs(int node, List<List<Integer>> adj, boolean[] suspicious) {
+        suspicious[node] = true;
+
+        for (int next : adj.get(node)) {
+            if (!suspicious[next]) {
+                dfs(next, adj, suspicious);
+            }
+        }
+    }
+
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge : invocations) {
+            adj.get(edge[0]).add(edge[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+        dfs(k, adj, suspicious);
+
+        for (int[] edge : invocations) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (!suspicious[u] && suspicious[v]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
